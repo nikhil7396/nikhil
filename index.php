c647bae43af91a8579b5ac5ff75051321c5edfe1
nikhil7396 <nikhil7396@gmail.com>
2015-08-21 01:53:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
 
 // change the following paths if necessary
-$yii='/opt/lampp/htdocs/qa/page.jsp';
-//$config=dirname(__FILE__).'/protected/config/main.php';
+$url='/opt/lampp/htdocs/qa/page.jsp';
 
-// remove the following lines when in production mode
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-//defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-require_once($yii);
+require_once($url);
 
 header(Location:'https://www.calcatraz.com/calculator/api?c=3%2B3');
 
-//Yii::createWebApplication($config)->run();
+
 ?>
